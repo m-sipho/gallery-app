e3adf7caa2afff0b04b8551da809018f15471206
m-sipho <241426803+m-sipho@users.noreply.github.com>
2026-02-28 10:53:25 +0200

    Add .env file and load all env variables

---
+++ backend/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, UploadFile, File, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
+import boto3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+aws_region_name = os.getenv("AWS_REGION_NAME")
+s3_bucket_name = os.getenv("S3_BUCKET_NAME")

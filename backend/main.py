b4ba1854b514345a9c211d52727f020243c7da4a
m-sipho <241426803+m-sipho@users.noreply.github.com>
2026-03-15 14:28:56 +0200

    Allow deployed frontend to communicate with API

--- backend/main.py
+++ backend/main.py
@@ -31,11 +31,12 @@ app = FastAPI()
 
 origins = [
     "http://localhost:5173",
+    "https://gallery-app-blue-ten.vercel.app/"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
